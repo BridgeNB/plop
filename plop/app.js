672c07d7970aa0c33839c7a6b942dd8fd7cf520d
BridgeNB <zyq123258789@gmail.com>
2016-12-04 15:57:17 -0500

    login page polished; MongoDB Database added

--- plop/app.js
+++ plop/app.js
@@ -1,34 +1,83 @@
+// packeage installation
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// login packages: mongodb passport
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash');
 
-// original //
-// var index = require('./routes/index');
-// var users = require('./routes/users');
-// original //
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// router control
+// login page temporary
+var home  = require('./routes/index');
+var login = require('./routes/login');
 
 var send  = require('./routes/send');
 var show  = require('./routes/show');
 
+
+// application
 var app = express();
 
-// view engine setup
+// view doc locations and view engine set up
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// dev log file
 app.use(logger('dev'));
+// jason explaination
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+// cookie parser
 app.use(cookieParser());
+////////////////add passport///////////////////
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(flash());
+app.use(passport.session());
+///////////////////////////////////////////////
+// static log file
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', show);
+// router file use
+// app.use('/', show);
+// app.use('/send', send);
+
+//////////////////////add passport////////////
+app.use('/', routes);
+app.use('/show', show);
 app.use('/send', send);
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+///////////////////////////////////////////////////
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -48,4 +97,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// export app module
 module.exports = app;
--- plop/routes/show.js
+++ plop/routes/show.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* Get index page */
 router.get('/', function(req, res, next) {
   // show the main page --- index.jade
-  res.render('index', {title:"plop"});
+  res.render('show', {title:"plop"});
 });
 
 module.exports = router;
